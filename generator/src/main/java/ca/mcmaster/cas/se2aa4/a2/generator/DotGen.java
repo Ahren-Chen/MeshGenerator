4381fca57a89c33b8e55c32616f84dc48f786cf7
Ahren Chen <ahren@hotmail.ca>
2023-02-04 18:03:25 -0500

    Updated DotGen to add a list of segments to display, next step is to display them

--- generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/DotGen.java
+++ generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/DotGen.java
@@ -1,13 +1,13 @@
 package ca.mcmaster.cas.se2aa4.a2.generator;
 
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.Random;
+import java.util.*;
 
+import ca.mcmaster.cas.se2aa4.a2.io.Structs;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Vertex;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Property;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Mesh;
+import ca.mcmaster.cas.se2aa4.a2.io.Structs.Segment;
 
 public class DotGen {
 
@@ -16,30 +16,51 @@ public class DotGen {
     private final int square_size = 20;
 
     public Mesh generate() {
-        Set<Vertex> vertices = new HashSet<>();
+        List<Vertex> vertices = new ArrayList<>();
+        List<Segment> segments = new ArrayList<>();
+
+
         // Create all the vertices
         for(int x = 0; x < width; x += square_size) {
             for(int y = 0; y < height; y += square_size) {
                 vertices.add(Vertex.newBuilder().setX((double) x).setY((double) y).build());
                 vertices.add(Vertex.newBuilder().setX((double) x+square_size).setY((double) y).build());
+
+                //segments.add(Segment.newBuilder().build());
+
                 vertices.add(Vertex.newBuilder().setX((double) x).setY((double) y+square_size).build());
                 vertices.add(Vertex.newBuilder().setX((double) x+square_size).setY((double) y+square_size).build());
             }
         }
+
         // Distribute colors randomly. Vertices are immutable, need to enrich them
-        Set<Vertex> verticesWithColors = new HashSet<>();
+        List<Vertex> verticesWithColors = new ArrayList<>();
         Random bag = new Random();
-        for(Vertex v: vertices){
+        //System.out.println(vertices);
+
+        for(int vertex = 0; vertex < vertices.size(); vertex++){
+            Vertex v = vertices.get(vertex);
+            //System.out.println("num: " + vertex + " " + vertices.get(vertex));
             int red = bag.nextInt(255);
             int green = bag.nextInt(255);
             int blue = bag.nextInt(255);
             String colorCode = red + "," + green + "," + blue;
+
             Property color = Property.newBuilder().setKey("rgb_color").setValue(colorCode).build();
             Vertex colored = Vertex.newBuilder(v).addProperties(color).build();
             verticesWithColors.add(colored);
-        }
 
-        return Mesh.newBuilder().addAllVertices(verticesWithColors).build();
+            if (vertex != 0) {
+                Segment segment = (Segment.newBuilder().setV1Idx(vertex - 1).setV2Idx(vertex).build());
+                Property segmentColor = Property.newBuilder().setKey("rgb_color")
+                        .setValue(colorCode).build();
+
+                Segment segmentColored = Segment.newBuilder(segment).addProperties(segmentColor).build();
+                segments.add(segmentColored);
+            }
+
+        }
+        return Mesh.newBuilder().addAllVertices(verticesWithColors).addAllSegments(segments).build();
     }
 
 }

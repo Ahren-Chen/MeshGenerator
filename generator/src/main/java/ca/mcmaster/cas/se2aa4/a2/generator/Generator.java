a3fabc640e234c116a876a9c426070130e8e7266
Shike Chen <40473396+MikeLoveGame@users.noreply.github.com>
2023-02-23 19:59:17 -0500

    Step 2 Done

--- generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/Generator.java
+++ generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/Generator.java
@@ -205,10 +205,10 @@ public class Generator {
         for (int i = 0; i < vertexList.size(); i++) {
             Vertex s0= vertexList.get(i);
             Segment s1=segmentList.get(i+1);
-            if (s0.compareTo(s1)==0){
+            /*if (s0.compareTo(s1)==0){
                 segmentList.remove(i);
                 i--;
-            }
+            }*/
         }
 
         return null;

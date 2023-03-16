35e5fc0b2537b2828d03959cc81667b500d5b3ae
Shike Chen <40473396+MikeLoveGame@users.noreply.github.com>
2023-03-15 21:58:54 -0400

    trying to solve neighbours not found bug

--- island/src/main/java/island/Interfaces/ShapeGen.java
+++ island/src/main/java/island/Interfaces/ShapeGen.java
@@ -3,5 +3,5 @@ package island.Interfaces;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs;
 
 public interface ShapeGen {
-    Structs.Mesh generate(Structs.Mesh mesh);
+    abstract Structs.Mesh generate(Structs.Mesh mesh);
 }

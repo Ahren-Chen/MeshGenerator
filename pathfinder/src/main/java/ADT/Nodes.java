1947f4f86c2d1780ce5685540c6a1578f7923bcb
Ahren Chen <ahren@hotmail.ca>
2023-04-04 22:29:44 -0400

    F58: Added x and y values to nodes

--- pathfinder/src/main/java/ADT/Nodes.java
+++ pathfinder/src/main/java/ADT/Nodes.java
@@ -6,12 +6,18 @@ public class Nodes {
     private String name;
     private List<Nodes> neighbors;
     private final double elevation;
+    private final double x;
+    private final double y;
 
-    public Nodes(double elevation) {
+    public Nodes(double elevation, double x, double y) {
         this.elevation = elevation;
+        this.x = x;
+        this.y = y;
     }
 
-    public Nodes() {
+    public Nodes(double x, double y) {
         this.elevation = 0;
+        this.x = x;
+        this.y = y;
     }
 }

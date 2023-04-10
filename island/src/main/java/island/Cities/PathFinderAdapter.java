3873f81eab6cd853d4aa4af64471a1ba61827704
Ahren Chen <ahren@hotmail.ca>
2023-04-09 20:50:38 -0400

    F61 & F62 created an adapter for pathfinder

---
+++ island/src/main/java/island/Cities/PathFinderAdapter.java
@@ -0,0 +1,52 @@
+package island.Cities;
+
+import ADT.Edges;
+import ADT.Nodes;
+import island.IOEncapsulation.Segment;
+import island.IOEncapsulation.Vertex;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PathFinderAdapter {
+    private final List<Nodes> nodeList;
+    private final List<Edges> edgeList;
+    public PathFinderAdapter(List<Vertex> vertexList, List<Segment> segmentList) {
+        nodeList = new ArrayList<>();
+        edgeList = new ArrayList<>();
+
+        Map<Vertex, Nodes> vertexNodesMap = new HashMap<>();
+
+        for (Vertex vertex : vertexList) {
+            double elevation = vertex.getElevation();
+            double x = vertex.getX();
+            double y = vertex.getY();
+            Nodes node = new Nodes(elevation, x, y);
+
+            nodeList.add(node);
+            vertexNodesMap.put(vertex, node);
+        }
+
+        for (Segment segment : segmentList) {
+            Vertex v1 = segment.getV1();
+            Vertex v2 = segment.getV2();
+
+            Nodes n1 = vertexNodesMap.get(v1);
+            Nodes n2 = vertexNodesMap.get(v2);
+
+            Edges edge = new Edges(n1, n2);
+
+            edgeList.add(edge);
+        }
+    }
+
+    public List<Nodes> getNodes() {
+        return nodeList;
+    }
+
+    public List<Edges> getEdges() {
+        return edgeList;
+    }
+}
